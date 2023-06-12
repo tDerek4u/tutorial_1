7f98d6ba0fe6b759512162b4fe7082840e2bd80a
tDerek4u <zwehtetmhan433@gmail.com>
2023-06-12 09:43:22 +0630

    pdf-to-images

---
+++ app/Http/Controllers/ConvertController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+use Imagick;
+use Spatie\PdfToImage\Pdf;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Org_Heigl\Ghostscript\Ghostscript;
+
+class ConvertController extends Controller
+{
+    public function index(){
+        // $version = Imagick::getVersion();
+        // dd($version);
+        // dd(phpinfo());
+        $pdf_file = public_path() . "\pdf\\test.pdf";
+        $output_path =  storage_path() . "/app/public/images";
+        Ghostscript::setGsPath("C:\Program Files\gs\gs10.01.1\bin\gswin64c.exe");
+        $pdf = new Pdf($pdf_file);
+
+        foreach (range(1, $pdf->getNumberOfPages()) as $pageNumber) {
+            $pageName = time().Str::random(6).'.'.'jpg';
+            $pdf->setPage($pageNumber)->saveImage($output_path.$pageName);
+        }
+
+
+    }
+}
